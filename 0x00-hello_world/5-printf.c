154d67aac0afefa65d135cf3580cbced3d32bc53
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-10 15:58:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#include <stdio.h> 
-#include <stdlib.h>  
-#include <time.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success
@@ -9,6 +9,6 @@ int main(void)
 {
 
 	srand(time(0));
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,);
 return (0);
 }
