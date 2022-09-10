67b980f9b1f1fdbfc32837abdc4d7b7b097641e6
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-10 14:50:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success
  */
 int main(void)
 {
-Puts ("\" Programming is like building a multingual puzzle");
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+Puts ("\"Programming is like building a multingual puzzle");
 return (0);
 }
