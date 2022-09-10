b8fff3926be013b1a8219c89ef7fe638c46d50ce
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-10 15:03:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-Puts ("\"Programming is like building a multingual puzzle");
+	puts("\"Programming is like building a multingual puzzle");
 return (0);
 }
