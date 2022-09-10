bf276db4eef514b873fc090537cab44352448d2d
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-10 03:24:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success
+ */
+int main(void)
+{
+Puts ("\" Programming is like building a multingual puzzle");
+return (0);
+}
