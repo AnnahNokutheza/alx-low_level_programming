500f0cbcbdae6b77440a32ae2c685ca272708276
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-10 18:11:45 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  *
  int main(void)
  {
-
- 	srand(time(0));
  printf("Size of a char: %d byte(s)\n", sizeof(char));
  printf("Size of an int: %d byte(s)\n", sizeof(int));
  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
