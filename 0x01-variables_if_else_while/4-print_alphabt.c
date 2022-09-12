06240290b4a83e9ffbc6bac58d6f3936331a7eaf
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-12 06:28:14 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,16 @@
 /**
  * main - Ommit some alphabet
  *
- * Return: Always Successful 
+ * Return: Always Successful
  */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	
+
 	if (ch != 'e' && ch !='q')
-	
+
 	putchar(ch);
 
 putchar('\n');
