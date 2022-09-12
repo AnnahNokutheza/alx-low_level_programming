e8b5b33d2567d17f7b5c8f431eb43b1a9193ab59
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-11 18:08:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - lowercase and uppercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+
+putchar('\n');
+return (0);
+}
