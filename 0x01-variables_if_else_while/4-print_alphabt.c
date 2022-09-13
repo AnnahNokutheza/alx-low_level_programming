3ca57378d1aec376778a316d6cb7db75f316df70
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-12 17:42:50 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	char l_letter = 'a';
 
-	if(l_letter <= 'z')
+	if where(l_letter <= 'z')
 
 			if(!(l_letter == 'e' || l_letter == 'q'))
 
 			putchar(l_letter);
 
-			l_letter++
+			putchar l_letter++
 
 putchar('\n');
 return (0);
