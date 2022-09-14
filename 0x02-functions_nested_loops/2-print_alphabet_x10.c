77cf3ce4b7c97728c5045a2a2fd8881146dd457d
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-14 16:22:07 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,11 @@ void print_alphabet_x10(void)
 
 char c, i;
 
-for  (i = 0; i <= 9; i++, '\n')
+for  (i = 0; i <= 9; i++)
 
-for (c = 'a'; c <= 'z'; c++, '\n')
+for (c = 'a'; c <= 'z'; c++)
 
 _putchar(c);
-
-i++;
-c++;
 
 _putchar('\n');
 
