3ee0af8389ea80a9d6b548b758059168860084e1
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-14 16:50:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,11 +10,12 @@ if (n > 0)
 _putchar('+');
 return (1);
 
-if else (n < 0)
-_putchar('-');
-return (-1);
+if else (n == 0)
 
-else 
 _putchar('0');
 return (0);
+
+else 
+_putchar('.');
+return (-1);
 }
