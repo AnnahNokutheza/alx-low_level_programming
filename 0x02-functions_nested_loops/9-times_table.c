e841fe9ac3e314fe65369133261265a4b6262d8b
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-14 20:50:49 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,18 +11,18 @@ void times_table(void)
 
 		_putchar('0');
 
-	for (mult = 1; mult <= 9; mult++)
+	if (mult = 1; mult <= 9; mult++)
 
 		_putchar('.');
 		_putchar(' ');
 
 				prod = num * mult;
 				if (prod <= 9)
-				_putchar(' ');
+				for _putchar(' ');
 				
 				else
 				_putchar((prod /10) + '0');
-				_putchar((prod % 10);
+				_putchar((prod % 10));
 
-					_putchar('\n')
+					_putchar('\n');
 }
