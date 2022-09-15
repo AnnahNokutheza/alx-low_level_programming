6be616359e31396c9b422feb6593b265033d96fa
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-14 20:06:07 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,8 +10,6 @@ void jack_bauer(void)
 
 	int hours = 0;
 	int minutes = 0;
-	int hours_reminder;
-	int mins_reminder;
 
 	while (hours <= 23)
 	{
@@ -20,7 +18,7 @@ void jack_bauer(void)
 
 		hours_reminder = hours % 10;
 		_putchar(hours / 10 + '0';
-		_putchar(hours_reminder + ('10')
+		_putchar(hours_reminder + ('0')
 		_puchar(':');
 		_putchar(minutes /10 + '0');
 		_putchar(mins_reminder + '0');
