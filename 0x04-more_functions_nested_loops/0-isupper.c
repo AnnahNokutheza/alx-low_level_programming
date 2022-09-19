3276b343b9b817ca546b258cb76f6391356e554c
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-18 20:21:59 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,15 @@
  *  _isupper - Check if a character is uppercase or not
  *  @c: character to be tested
  *
- *  Return: 1 whether it is, 0 otherwise
+ *  Return: 1 for upper letter or 0 for any else
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
 
 	return (0);
 }
+
