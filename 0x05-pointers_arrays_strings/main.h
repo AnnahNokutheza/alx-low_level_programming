0a8e66cf39932c9a58ff631e4a5978d1a31006d1
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-19 18:24:21 -0700

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
-int _putchar(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
