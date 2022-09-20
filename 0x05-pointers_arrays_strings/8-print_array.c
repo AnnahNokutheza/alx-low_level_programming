6412554b22dc0dd65176e066ef1421bcf165e5d7
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-19 20:58:39 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 
 /**
  * prints_array - Prints n elements of an array of integers
- * @a: elements of an array of integers
+ * @a: Print array
  * @n: Number of elements of the array to be printed
  *
  * Return: void
