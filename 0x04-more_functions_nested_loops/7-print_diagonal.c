fb79d2cf8608079ad4c49b789a30bb3a91a23368
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-21 14:36:49 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * print_square - Prints n squares according n number of times
- * @size: The number of squares/number of times
+ * print_diagonal - Draws a diagonal lines according parameter
+ * @n: The number of times to print diagonal lines
  *
- * Return: empty
+ *  Return: empty
  */
 void print_diagonal(int n)
 {
-int x, y;
+	int x, y;
 
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (x = 0; x < n; x++)
-{
-for (y = 0; y < x; y++)
-{
-_putchar(32);
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
 
-_putchar(92);
-_putchar('\n');
-}
-}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
