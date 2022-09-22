ca7ca0130e3a32d8c173a67185a5730614d0e255
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-09-21 22:34:14 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,28 @@
+#include "main.h"
 #include <stdio.h>
-#include <string.h>
 
-int main()
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
 {
-char destination[] = "Hello ";
-char source[] = "World!";
-strcat(destination,source);
-printf("Concatenated String: %s\n", strcat(destination,source));
-return 0;
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
 }
