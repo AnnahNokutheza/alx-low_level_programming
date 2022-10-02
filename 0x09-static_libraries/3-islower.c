5f6eca1ccc0b1d18ff03a45d3287d9d69b1db24e
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-02 16:42:25 -0700

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_islower - checks for lowercase
+ * @c: is the int that will use for the argument of the function
+ * Return: 1 if c is lowercase, 0 otherrwisie
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @c: input letrer
+ * Return: Always successs
+ */
+int _abs(int c)
+{
+
+if (c > 0 || c == 0)
+{
+	return (c);
+}
+else
+	return (c * -1);
+}
