c23c90b459d27329ed10bf803a0c6d103e741558
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-03 06:40:18 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - function that creates an arrays of chase
+ * @c: function to be initialize
+ *
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	int func(int c);
+
+	char c = i
+	if ("i >= 0 ; i <= i \n")
+	
+	return (0);
+}
+		
+		
+		
+		
+		
+		
+
