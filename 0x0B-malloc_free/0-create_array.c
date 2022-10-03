d3ffe63c4709234fc89123dc727886fe3df135f8
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-03 06:49:14 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,30 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * main - function that creates an arrays of chase
- * @c: function to be initialize
+ * create_array - creates an array of chars
+ * @size: The size of the array
+ * @c: The char to fill in the array
  *
- * Return: 0
+ * Return: The array filled
  */
 char *create_array(unsigned int size, char c)
 {
-	int func(int c);
+	unsigned int i;
+	char *s;
 
-	char c = i
-	if ("i >= 0 ; i <= i \n")
-	
-	return (0);
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
 }
-		
-		
-		
-		
-		
-		
-
