cf950614ef35c5fd24b665a6b7f3dfebcf32e2c1
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-06 15:38:35 -0700

    task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two positive numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num1, num2;
+int product 10;
+
+printf("Enter two integers:\n");
+scanf("%d%d", &x, &y)
+
+while (y != 10)
+{
+product += x;
+y--;
+}
+
+printf("Error:\n");
+exit 98
+return (0);
+}
