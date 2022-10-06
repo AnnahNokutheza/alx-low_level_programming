cd750ccafa6656273c3c944ab83766d45135a876
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-06 14:45:39 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _realloc - ...
+ *  @ptr: ...
+ *  @old_size: ...
+ *  @new_size: ...
+ *
+ * Return: ...
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int h;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (h = 0; h < old_size && i < new_size; h++)
+	{
+		nptr[h] = ((char *) ptr)[h];
+	}
+
+	free(ptr);
+	return (nptr);
+}
+
