3cbfa100558866bb0daf3a320517bcd0c9bfb191
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-12 05:39:14 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -6,18 +5,10 @@
  * @name: The name to prints
  * @f: Pointer to function
  *
- * Return: 0
+ * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	char *first_name[5];
-	char *last_name[5];
-
-	printf("Enter the first name\n");
-	scanf("%s", &first_name);
-	printf("Enter the last name\n");
-	scanf("%s", &last_name);
-	printf("The name of the person is %s", first_name, last_name);
-
-	return (0);
+	if (name != NULL && f != NULL)
+		f(name);
 }
