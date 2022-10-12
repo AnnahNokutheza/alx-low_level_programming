0323ea81c2f9a57a49a631b9a93e55b39dc598bb
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-11 23:30:30 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Dog attributes
@@ -16,13 +16,10 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_annh - Typedef for dog structure
- */
-typedef struct dog dog_annh;
-
+typedef struct dog dog_t;
 void init_dog(struct dog *ptr, char *name, float age, char *owner);
 void print_dog(struct dog *ptr);
 dog_annh *new_dog(char *name, float age, char *owner);
 void free_dog(dog_annh *ptr);
-#endif
+
+#endif /*DOG_H*/
