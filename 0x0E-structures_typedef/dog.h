36210e0f9cea0849870f2b88b9fb8af2ad3c48b8
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-11 23:44:02 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,9 @@ struct dog
 };
 
 typedef struct dog dog_t;
-void init_dog(struct dog *ptr, char *name, float age, char *owner);
-void print_dog(struct dog *ptr);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_annh *new_dog(char *name, float age, char *owner);
-void free_dog(dog_annh *ptr);
+void free_dog(dog_annh *d);
 
 #endif /*DOG_H*/
