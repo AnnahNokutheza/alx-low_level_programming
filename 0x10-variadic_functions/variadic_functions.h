e226c7f2e4604220212dcb87429deee3b15662e8
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-13 09:19:55 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
-#include <stddef.h>                                                                                          
-#ifndef MAIN_H                                                                                               
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>                                                                                          
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
