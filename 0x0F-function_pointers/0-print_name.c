8b608ee20e6868be2172448caf9048b6f4b10c66
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-14 16:20:24 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
-#include "function_pointers.h"
-
 /**
  *  print_name - Prints a name
- *  @name: The name to prints
- *  @f: Pointer to function
+ *  @name: pointer to name
+ *  @f: function to pointer
  *
- *  Return: Nothing
+ *  Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
+if (name && f)
 f(name);
 }
