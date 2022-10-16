a662b36bd12ed3b137b327dc2cd72c5a734873a0
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-16 01:37:39 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,9 +3,9 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- *  @n: The number to start printing from
+ * @n: The number to start printing from
  *
- *  Return: Always 0
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
@@ -26,17 +26,14 @@ void print_to_98(int n)
 }
 else
 {
-for (; n >= 98; n--)
-{
-	if (n == 98)
+	for (; n >= 98; n--)
 	{
-		printf("%d", n);
-		printf("\n");
-		break;
-	}
-	else
-	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			printf("\n");
+			break;
+		}
 		printf("%d, ", n);
 	}
-}
 }
