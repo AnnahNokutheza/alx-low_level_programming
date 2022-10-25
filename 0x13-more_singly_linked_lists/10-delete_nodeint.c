196c9a03151f91c3948d130b4b6e165d0f3e65a5
Annah Nokutheza Mwanza <110993039+AnnahNokutheza@users.noreply.github.com>
2022-10-25 08:28:12 -0300

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - ...
+  * @head: ...
+  * @index: ...
+  *
+  * Return: ...
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int #include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - ...
+  * @head: ...
+  * @index: ...
+  *
+  * Return: ...
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int enumerate = 1;
+	listint_t *new = *head, *better;
+
+	if (!head || !*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = new->next;
+		free(new);
+		return (1);
+	}
+
+	better = *head;
+	while (better)
+	{
+		if (enumerate == index)
+		{
+			new = better->next;
+			better->next = new->next;
+			free(new);
+			return (1);
+		}
