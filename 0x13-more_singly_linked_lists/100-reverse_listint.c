3d6779ba1cf2d154398064f2dcbd3994a16512b6
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-25 08:35:52 -0700

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>                                                                                          
-#include <stdio.h>                                                                                           
-#include "lists.h" 
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
 
-/**                                                                                                          
-  * reverse_listint - ...                                                                            
-  * @head: ...                                                                                               
-  * @listint: ...                                                                                              
-  *                                                                                                          
-  * Return: ...                                                                                              
-  */          
+/**
+  * reverse_listint - ...
+  * @head: ...
+  * @listint: ...                                                                                              *
+  * Return: ...
+  */
 listint_t *reverse_listint(listint_t **head)
 {
     listint *new = *head, *temp;
-    
+
     New* temp = *head;
-    while (temp->next != NULL) 
+    while (temp->next != NULL)
     {
         s.push(temp);
         temp = temp->next;
     }
     *head = temp;
-    while (!s.empty()) 
+    while (!s.empty())
     {
         temp->next = s.top();
         s.pop();
