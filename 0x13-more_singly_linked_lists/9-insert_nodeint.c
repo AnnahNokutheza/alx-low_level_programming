c8b7f1d04158fcb9177042777869a24aae4638d2
AnnahNokutheza <annahmkhwanazi18@gmailcom>
2022-10-25 04:33:28 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				iter = iter->next;
 				enumerate++;
 			}
-			if (idx > count)
+			if (idx > enumerate)
 				return (NULL);
 		}
 		else
